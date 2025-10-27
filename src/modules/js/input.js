b63d17b8a77fb32c748e024c88bc1a259edf5541
fnzh-i <fnzh.4u@gmail.com>
2025-10-28 01:35:18 +0800

    Add input validation for numeric and length checks
    + Introduced validation to ensure inputs are numeric, non-empty, of equal length, and that burst times are not zero.
    + User feedback is provided for invalid inputs, and errors are thrown to prevent further processing.

--- src/modules/js/input.js
+++ src/modules/js/input.js
@@ -8,6 +8,24 @@ export function inputElements(){
 
     const arrTA = [...arrT].map(Number);
     const burTA = [...burT].map(Number);
+
+    if (burTA.includes(NaN) || arrTA.includes(NaN)) {
+        submits.textContent = "Please enter valid numbers only.";
+        throw new Error("Invalid input: Non-numeric values detected.");
+    }
+    else if (arrTA.length === 0 || burTA.length === 0) {
+        submits.textContent = "Please enter at least two or more number in each input.";
+        throw new Error("Invalid input: Empty input detected.");
+    }
+    else if (arrTA.length !== burTA.length) {
+        submits.textContent = "Both inputs needs to have the same length";
+        throw new Error("Invalid input: Mismatched input lengths.");
+    }
+    else if (burTA.includes(0)) {
+        window.alert("Burst Time cannot have 0.");
+        throw new Error("Invalid input: Burst Time cannot have 0.");
+    }
+
     
     return { arrT, burT, arrTA, burTA, outputProcess, outputs, submits };
 }
